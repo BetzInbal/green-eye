f54b5605b215d6f2de10eb9e93f3a2f6c52c951c
betz <betzinbal@gmail.com>
2024-10-07 17:23:40 +0300

    middlewares complited

--- src/middlewares/authMiddlewares.js
+++ src/middlewares/authMiddlewares.js
@@ -4,19 +4,28 @@ const { models } = require("mongoose")
 
 const onlyCommanders = async (req,res,next) => {
     try {
-        
+        const token = req.cookies.token
+        const userData = await jwt.verify(token, process.env.TOP_SECRET)
+        req.user = userData
+        if (userData.role != 'commander'){
+            res.status(403)
+            .json('You are far too simple')
+        }
+        next()
     } catch (error) {
-        
+        console.log(error);
+        res.status(401).json(error)
     }
 }
 
 const onlySoldiersAndCommanders = async (req,res,next) => {
     try {
         const token = req.cookies.token
-        const userData = await jet.verify(token, process.env.TOP_SECRET)
+        const userData = await jwt.verify(token, process.env.TOP_SECRET)
         req.user = userData
         next()
     } catch (error) {
+        console.log(error);
         res.status(401).json(error)
     }
 }
