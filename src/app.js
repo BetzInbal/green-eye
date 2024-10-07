71b84b3a9f5a253b72659b4f2afa6d9c17c51f6f
betz <betzinbal@gmail.com>
2024-10-07 10:32:33 +0300

    Connecting a router to the APP

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log("sfdd");
+const exp = require('express')
+const dotenv = require('dotenv')
+
+const app = exp()
+dotenv.config()
+
+const port = process.env.PORT || 1415
+
+app.use(exp.json())
+app.use('/auth', require('./routes/authRouter'))
+app.use('/user', require('./routes/userRouter'))
+app.use('/greenEye', require('./routes/greenEyeRouter'))
+
+
+
+
+app.listen(port,() =>{
+    console.log(`server runing on ${port}`);
+    
+})
